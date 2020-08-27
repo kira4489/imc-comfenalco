123952b1caa1c0a5a95767ed91b51eedd79caa44
jhonatanquirama <quiramapino47@gmail.com>
2020-08-27 12:54:40 -0500

    Updates

---
+++ precache-manifest.c22c93c2872212461362f75706095e29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5be00a725dcadc5e486f2cac204fee2",
+    "url": "/imc-comfenalco/index.html"
+  },
+  {
+    "revision": "ffab53a8fa7e3933c1db",
+    "url": "/imc-comfenalco/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9efa4a4d153f95a50dea",
+    "url": "/imc-comfenalco/static/js/2.0f205447.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imc-comfenalco/static/js/2.0f205447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffab53a8fa7e3933c1db",
+    "url": "/imc-comfenalco/static/js/main.e2fd3dd1.chunk.js"
+  },
+  {
+    "revision": "167627408a2ee8a60fa7",
+    "url": "/imc-comfenalco/static/js/runtime-main.7df8f07e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/imc-comfenalco/static/media/logo.5d5d9eef.svg"
+  }
+]);
